bf8ee67e550a90e69be290586bc7a52db6b8d36b
ben-san-57 <santaniello.benjamin@gmail.com>
2025-05-19 11:12:31 +0200

    first commit

---
+++ media_item.py.py
@@ -0,0 +1,24 @@
+# media_item.py
+from abc import ABC, abstractmethod
+
+class MediaItem(ABC):
+    def __init__(self, title, duration):
+        self._title = title
+        self._duration = duration  # en secondes
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def duration(self):
+        return self._duration
+
+    def get_formatted_duration(self):
+        mins = self._duration // 60
+        secs = self._duration % 60
+        return f"{mins:02d}:{secs:02d}"
+
+    @abstractmethod
+    def info(self):
+        pass
---
+++ music_track.py.py
@@ -0,0 +1,29 @@
+# music_track.py
+from media_item import MediaItem
+
+class MusicTrack(MediaItem):
+    def __init__(self, path, title, artist, album, genre, duration):
+        super().__init__(title, duration)
+        self._path = path
+        self._artist = artist
+        self._album = album
+        self._genre = genre
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def artist(self):
+        return self._artist
+
+    @property
+    def album(self):
+        return self._album
+
+    @property
+    def genre(self):
+        return self._genre or "Inconnu"
+
+    def info(self):
+        return f"{self.title} - {self.artist} ({self.album}, {self.genre}) [{self.get_formatted_duration()}]"
